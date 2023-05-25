20e5f051f088d5fe3eeaeedb1f436040326e5619
tallenintegsys <tallen@integsys.biz>
2023-05-24 23:06:38 -0400

    I need some kind of sequencer

--- verilog/termbuffer.v
+++ verilog/termbuffer.v
@@ -10,6 +10,7 @@ module termbuffer (
 localparam CMD_NONE = 0;
 localparam CMD_CURSOR = 1;
 localparam CMD_REFRESH = 2;
+localparam CMD_TAB = 3;
 reg [1:0] cmd = CMD_NONE;
 
 localparam CURSOR0 = 0;
@@ -20,7 +21,12 @@ reg [1:0] cursor = CURSOR0;
 localparam REFRESH0 = 0;
 localparam REFRESH1 = 1;
 localparam REFRESH2 = 2;
-reg [3:0] refresh = REFRESH0;
+reg [1:0] refresh = REFRESH0;
+
+localparam TAB0 = 0;
+localparam TAB1 = 1;
+localparam TAB2 = 2;
+reg [3:0] tab = TAB0;
 
 // text buffer
 reg  tb_wen = 0;
@@ -80,7 +86,66 @@ always @(posedge clk) begin
             refresh <= REFRESH0;
             if (tb_addr == 271) cmd <= CMD_NONE;
        end
+       default:
+            refresh <= REFRESH0;
        endcase
+    end else if (cmd == CMD_TAB) begin
+        case (tab)
+        TAB0: begin
+            o_byte <= "\033";
+            o_byte_v <= 1;
+            tab <= TAB1;
+        end
+        TAB1: begin
+            o_byte_v <= 0;
+            tab < TAB2;
+        end
+        TAB2: begin
+            o_byte <= "[";
+            o_byte_v <= 1;
+            tab <= TAB3;
+        end
+        TAB3: begin
+            o_byte_v <= 0;
+            tab < TAB4;
+        end
+        TAB4: begin
+            o_byte <= "\033";
+            o_byte_v <= 1;
+            tab <= TAB5;
+        end
+        TAB5: begin
+            o_byte_v <= 0;
+            tab < TAB6;
+        end
+        TAB6: begin
+            o_byte <= "\033";
+            o_byte_v <= 1;
+            tab <= TAB7;
+        end
+        TAB7: begin
+            o_byte_v <= 0;
+            tab < TAB8;
+        end
+        TAB8: begin
+            o_byte <= "\033";
+            o_byte_v <= 1;
+            tab <= TAB9;
+        end
+        TAB9: begin
+            o_byte_v <= 0;
+            tab < TAB10;
+        end
+        TAB10: begin
+            o_byte <= "\033";
+            o_byte_v <= 1;
+            tab <= TAB11;
+        end
+        TAB11: begin
+            o_byte_v <= 0;
+            tab < TAB2;
+        end
+        endcase
     end else if (i_byte_v) begin
         case (i_byte)
         "j": begin //down
