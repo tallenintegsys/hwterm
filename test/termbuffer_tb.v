3f45b8cd00dec0b066be2f3224f6bc48326c09b6
tallenintegsys <tallen@integsys.biz>
2023-05-24 21:46:22 -0400

    this feels cumbersome

--- test/termbuffer_tb.v
+++ test/termbuffer_tb.v
@@ -4,6 +4,7 @@ reg         clk             = 0;
 reg         rst             = 0;
 wire [7:0]  o_byte;
 wire        o_byte_v;
+reg         i_byte_done = 0;
 reg [7:0]   i_byte = 0;
 reg         i_byte_v = 0;
 
@@ -12,6 +13,7 @@ termbuffer uut (
     .rst,
     .o_byte,
     .o_byte_v,
+    .i_byte_done,
     .i_byte,
     .i_byte_v);
 
@@ -21,46 +23,41 @@ initial begin
     $dumpvars;
     rst = 1;
     #50 rst = 0;
+
+    /*
     #10 i_byte = "l";
     for (i=0; i < 1024; i = i + 1) begin
         #20 i_byte_v = 1;
         #20 i_byte_v = 0;
         while (o_byte_v == 0) #1;
-        if (o_byte == 8'h1b)
-            $write("%c%c%c",8'he2,8'h90,8'h9b);
-        else if (o_byte == 8'h0d)
-            $write("%c", o_byte);
-        else if (o_byte == 8'h0a)
-            $write("%c", o_byte);
-        else if (o_byte == 8'h20)
-            $write("%c", o_byte);
-        else if (o_byte < "0")
-            $write("%x", o_byte);
-        else
-            $write("%c", o_byte);
+        if (o_byte == 8'h1b) $write("%c%c%c",8'he2,8'h90,8'h9b);
+        else if (o_byte == 8'h0d) $write("%c", o_byte);
+        else if (o_byte == 8'h0a) $write("%c", o_byte);
+        else if (o_byte == 8'h20) $write("%c", o_byte);
+        else if (o_byte < "0") $write("%x", o_byte);
+        else $write("%c", o_byte);
+        #10 i_byte_done = 1;
+        #10 i_byte_done = 0;
     end
-    /*
+*/
     i_byte = " ";
     #1 i_byte_v = 1;
     #1 i_byte_v = 0;
-    for (i=0; i < 1024; i = i + 1) begin
-        #4
-        if (o_byte == 8'h1b)
-            $write("%c%c%c",8'he2,8'h90,8'h9b);
-        else if (o_byte == 8'h0d)
-            $write("%c", o_byte);
-        else if (o_byte == 8'h0a)
-            $write("%c", o_byte);
-        else if (o_byte == 8'h20)
-            $write("%c", o_byte);
-        else if (o_byte < "0")
-            $write("%x", o_byte);
-        else
-            $write("%c", o_byte);
+    for (i=0; i < 271; i = i + 1) begin
+        while (o_byte_v == 0) #1;
+        //if (o_byte == 8'h1b) $write("%c%c%c",8'he2,8'h90,8'h9b);
+        if (o_byte == 8'h1b) $write("%c", o_byte);
+        else if (o_byte == 8'h0d) $write("%c", o_byte);
+        else if (o_byte == 8'h0a) $write("%c", o_byte);
+        else if (o_byte == 8'h20) $write("%c", o_byte);
+        else if (o_byte < "0") $write("%x", o_byte);
+        else $write("%c", o_byte);
+        #10 i_byte_done = 1;
+        #10 i_byte_done = 0;
     end
-    */
+
     $display();
-    $display("%c%c%c%c",8'hf0,8'h9f,8'h8d,8'hba);
+    $display("%c%c%c%c",8'hf0,8'h9f,8'h8d,8'hba); // beer
     $finish;
 end // initial
 
--- verilog/termbuffer.v
+++ verilog/termbuffer.v
@@ -3,8 +3,24 @@ module termbuffer (
 	input	            rst,
 	output reg [7:0]    o_byte,	// serial out
     output reg          o_byte_v,
+    input               i_byte_done,
 	input [7:0]         i_byte,  // serial in
 	input	            i_byte_v);
+
+localparam CMD_NONE = 0;
+localparam CMD_CURSOR = 1;
+localparam CMD_REFRESH = 2;
+reg [1:0] cmd = CMD_NONE;
+
+localparam CURSOR0 = 0;
+localparam CURSOR1 = 1;
+localparam CURSOR2 = 2;
+reg [1:0] cursor = CURSOR0;
+
+localparam REFRESH0 = 0;
+localparam REFRESH1 = 1;
+localparam REFRESH2 = 2;
+reg [3:0] refresh = REFRESH0;
 
 // text buffer
 reg  tb_wen = 0;
@@ -20,7 +36,6 @@ buffer text_buffer0 (
     .rdata(tb_rdata));
 
 reg	 [9:0]cursor_ptr = 10'd288;
-reg  refresh = 0;
 reg  [2:0]send = 0;
 
 always @(posedge clk) begin
@@ -31,44 +46,71 @@ always @(posedge clk) begin
         cursor_ptr <= 0;
         o_byte_v <= 0;
         o_byte <= 0;
-    end else if (send != 0) begin // process output
-        case (send)
-        1: begin
+    end else if (cmd == CMD_CURSOR) begin
+        case (cursor)
+        CURSOR0: begin
             o_byte <= tb_rdata;
             o_byte_v <= 1;
-            send <= 2;
+            cursor <= CURSOR1;
         end
-        2: begin
+        CURSOR1: begin
             o_byte_v <= 0;
-            send <= 0;
+            if (i_byte_done) cursor <= CURSOR2;
         end
+        CURSOR2: begin
+            cmd <= CMD_NONE;
+            cursor <= CURSOR0;
+        end
+        default:
+            cursor <= CURSOR0;
         endcase
+    end else if (cmd == CMD_REFRESH) begin
+        case (refresh)
+        REFRESH0: begin
+            o_byte <= tb_rdata;
+            o_byte_v <= 1;
+            refresh <= REFRESH1;
+        end
+        REFRESH1: begin
+            o_byte_v <= 0;
+            if (i_byte_done) refresh <= REFRESH2;
+        end
+        REFRESH2: begin
+            tb_addr <= tb_addr + 1;
+            refresh <= REFRESH0;
+            if (tb_addr == 271) cmd <= CMD_NONE;
+       end
+       endcase
     end else if (i_byte_v) begin
         case (i_byte)
         "j": begin //down
             cursor_ptr <= cursor_ptr + 10'd40;
             tb_addr <= cursor_ptr;
+            cmd <= CMD_CURSOR;
         end
         "k": begin //up
             cursor_ptr <= cursor_ptr - 10'd40;
             tb_addr <= cursor_ptr;
+            cmd <= CMD_CURSOR;
         end
         "h": begin //left
             cursor_ptr <= cursor_ptr - 10'd1;
             tb_addr <= cursor_ptr;
+            cmd <= CMD_CURSOR;
         end
         "l": begin //right
             cursor_ptr <= cursor_ptr + 10'd1;
             tb_addr <= cursor_ptr;
+            cmd <= CMD_CURSOR;
         end
-        " ": begin //right
-        //XXX TBD
+        " ": begin //space
+            tb_addr <= 0;
+            cmd <= CMD_REFRESH;
         end
         default: begin
                 //do nothing
         end
         endcase
-        send <= 1;
     end // if
 end // always
 endmodule
