7b860dec8b3dd26b46e214dee218f6e52d5a6177
tallenintegsys <tallen@integsys.biz>
2023-05-24 23:05:55 -0400

    update UI

--- ui.sh
+++ ui.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-echo -e "                     ________________   "
-echo -e "                    |                |  "
-echo -e "  delay  width      | pulse  width   |  "
-echo -e "  \e[100m            \e[m ns   | \e[100m            \e[m ns|  "
-echo -e "____________________|                |__"
-echo 
+echo -e "                         ____________   "
+echo -e "                        |   pulse    |  "
+echo -e "   delay  width         |   width    |  "
+echo -e " \e[100m                 \e[m ns   | \e[100m        \e[m ns|  "
+echo -e "________________________|            |__"
+echo
 echo -e " tab=field space=redraw enter=trigger   "
+echo -en "\e[4A\e[3G\e[100m"
+read -n13 
+echo -e "\e[m"
 exit
 
 echo -e "                     ________________   "
