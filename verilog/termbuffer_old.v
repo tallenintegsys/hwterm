a4f5f3abc565c6cb510d45f1f0fbe30de99984e0
tallenintegsys <tallen@integsys.biz>
2025-04-30 21:27:31 -0400

    just echo for now tx and rx work

--- verilog/termbuffer_old.v
+++ verilog/termbuffer_old.v
@@ -3,8 +3,7 @@ module termbuffer (
 	input	            rst,
 	output reg [7:0]    o_byte,	// serial out
     output reg          o_byte_v,
-	input				i_tx_active,
-	input				i_tx_done,
+    input               i_byte_done,
 	input [7:0]         i_byte,  // serial in
 	input	            i_byte_v);
 
@@ -12,8 +11,7 @@ localparam CMD_NONE = 0;
 localparam CMD_CURSOR = 1;
 localparam CMD_REFRESH = 2;
 localparam CMD_TAB = 3;
-localparam CMD_ECHO = 4;
-reg [2:0] cmd = CMD_ECHO;
+reg [1:0] cmd = CMD_NONE;
 
 localparam CURSOR0 = 0;
 localparam CURSOR1 = 1;
@@ -22,27 +20,13 @@ reg [1:0] cursor = CURSOR0;
 
 localparam REFRESH0 = 0;
 localparam REFRESH1 = 1;
-reg refresh = REFRESH0;
+localparam REFRESH2 = 2;
+reg [1:0] refresh = REFRESH0;
 
 localparam TAB0 = 0;
 localparam TAB1 = 1;
 localparam TAB2 = 2;
-localparam TAB3 = 3;
-localparam TAB4 = 4;
-localparam TAB5 = 5;
-localparam TAB6 = 6;
-localparam TAB7 = 7;
-localparam TAB8 = 8;
-localparam TAB9 = 9;
-localparam TAB10 = 10;
-localparam TAB11 = 11;
 reg [3:0] tab = TAB0;
-
-localparam ECHO0 = 0;
-localparam ECHO1 = 1;
-localparam ECHO2 = 2;
-localparam ECHO3 = 3;
-reg [1:0] echo = ECHO0;
 
 // text buffer
 reg  tb_wen = 0;
@@ -77,7 +61,7 @@ always @(posedge clk) begin
         end
         CURSOR1: begin
             o_byte_v <= 0;
-            cursor <= CURSOR2;
+            if (i_byte_done) cursor <= CURSOR2;
         end
         CURSOR2: begin
             cmd <= CMD_NONE;
@@ -88,20 +72,23 @@ always @(posedge clk) begin
         endcase
     end else if (cmd == CMD_REFRESH) begin
         case (refresh)
-		REFRESH0: begin
-			o_byte <= tb_rdata;
-			o_byte_v <= 1;
-			refresh <= REFRESH1;
-		end
-		REFRESH1: begin
-			o_byte_v <= 0;
-			if (i_tx_done) begin
-				refresh <= REFRESH0;
-				tb_addr <= tb_addr + 1;
-				if (tb_addr == 0) cmd <= CMD_NONE;
-			end
-		end
-		endcase
+        REFRESH0: begin
+            o_byte <= tb_rdata;
+            o_byte_v <= 1;
+            refresh <= REFRESH1;
+        end
+        REFRESH1: begin
+            o_byte_v <= 0;
+            if (i_byte_done) refresh <= REFRESH2;
+        end
+        REFRESH2: begin
+            tb_addr <= tb_addr + 1;
+            refresh <= REFRESH0;
+            if (tb_addr == 271) cmd <= CMD_NONE;
+       end
+       default:
+            refresh <= REFRESH0;
+       endcase
     end else if (cmd == CMD_TAB) begin
         case (tab)
         TAB0: begin
@@ -111,7 +98,7 @@ always @(posedge clk) begin
         end
         TAB1: begin
             o_byte_v <= 0;
-            if (i_tx_done) tab <= TAB2;
+            tab < TAB2;
         end
         TAB2: begin
             o_byte <= "[";
@@ -120,7 +107,7 @@ always @(posedge clk) begin
         end
         TAB3: begin
             o_byte_v <= 0;
-            if (i_tx_done) tab <= TAB4;
+            tab < TAB4;
         end
         TAB4: begin
             o_byte <= "\033";
@@ -129,7 +116,7 @@ always @(posedge clk) begin
         end
         TAB5: begin
             o_byte_v <= 0;
-            if (i_tx_done) tab <= TAB6;
+            tab < TAB6;
         end
         TAB6: begin
             o_byte <= "\033";
@@ -138,7 +125,7 @@ always @(posedge clk) begin
         end
         TAB7: begin
             o_byte_v <= 0;
-            if (i_tx_done) tab <= TAB8;
+            tab < TAB8;
         end
         TAB8: begin
             o_byte <= "\033";
@@ -147,7 +134,7 @@ always @(posedge clk) begin
         end
         TAB9: begin
             o_byte_v <= 0;
-            if (i_tx_done) tab <= TAB10;
+            tab < TAB10;
         end
         TAB10: begin
             o_byte <= "\033";
@@ -156,25 +143,9 @@ always @(posedge clk) begin
         end
         TAB11: begin
             o_byte_v <= 0;
-            if (i_tx_done) tab <= TAB2;
+            tab < TAB2;
         end
         endcase
-    end else if (cmd == CMD_ECHO) begin
-        case (echo)
-        ECHO0: begin
-			if (i_byte_v) begin
-				o_byte <= i_byte;
-				o_byte_v <= 1;
-				echo <= ECHO1;
-			end
-        end
-		ECHO1: begin
-			o_byte_v <= 0;
-			if (i_tx_done) echo <= ECHO0;
-		end
-		default:
-			echo <= ECHO0;
-		endcase
     end else if (i_byte_v) begin
         case (i_byte)
         "j": begin //down
